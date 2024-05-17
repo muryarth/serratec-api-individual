91019341ff33772cee57635e04c2eb6eeab0fdf8
muryarth <arthur.mury@outlook.com>
2024-05-16 23:40:00 -0300

    feat: subindo classes para tratamento de erros

---
+++ individual/src/main/java/org/serratec/api/individual/exception/ErroResposta.java
@@ -0,0 +1,52 @@
+package org.serratec.api.individual.exception;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class ErroResposta {
+	private Integer status;
+	private String titulo;
+	private LocalDateTime horario;
+	private List<String> erros;
+
+	public ErroResposta(Integer status, String titulo, LocalDateTime horario, List<String> erros) {
+		super();
+		this.status = status;
+		this.titulo = titulo;
+		this.horario = horario;
+		this.erros = erros;
+	}
+
+	public Integer getStatus() {
+		return status;
+	}
+
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public LocalDateTime getHorario() {
+		return horario;
+	}
+
+	public void setHorario(LocalDateTime horario) {
+		this.horario = horario;
+	}
+
+	public List<String> getErros() {
+		return erros;
+	}
+
+	public void setErros(List<String> erros) {
+		this.erros = erros;
+	}
+
+}
